d5a57f733ba807a3503f829f25d779304425ef15
Leo Iannacone <l3on@ubuntu.com>
2014-02-23 20:09:28 +0100

    added icon references

--- workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/gui/DefaultValues.java
+++ workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/gui/DefaultValues.java
@@ -1,5 +1,16 @@
 package it.unibo.games.rpsls.gui;
 
+import java.awt.Color;
+
 public class DefaultValues {
-	public static int LIGHT_COLOR = 666666666;
+	public static Color LIGHT_COLOR = Color.LIGHT_GRAY;
+	
+	private static String basePathImage = "data/";
+	
+	public static String ICON_ROCK = basePathImage + "rock.png";
+	public static String ICON_PAPER = basePathImage + "paper.png";
+	public static String ICON_SCISSORS = basePathImage + "scissors.png";
+	public static String ICON_LIZARD = basePathImage + "lizard.png";
+	public static String ICON_SPOCK = basePathImage + "spock.png";
+
 }
