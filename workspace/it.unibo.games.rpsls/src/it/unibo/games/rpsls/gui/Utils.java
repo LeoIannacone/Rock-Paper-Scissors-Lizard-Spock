36107d942484ff46f6242bb11a96296a49be1dc0
Leo Iannacone <l3on@ubuntu.com>
2014-02-23 22:58:56 +0100

    some utils function

---
+++ workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/gui/Utils.java
@@ -0,0 +1,29 @@
+package it.unibo.games.rpsls.gui;
+
+import java.io.File;
+
+import it.unibo.games.rpsls.interfaces.IHit;
+
+public class Utils {
+	
+	public static File getHitButtonIcon(IHit hit) {
+		return getIcon(hit.getName(), 50);
+	}
+	
+	public static File getHitPanelIcon(IHit hit) {
+		return getIcon(hit.getName(), 96);
+	}
+	public static File getHitPanelIconBlank() {
+		return getIcon("hit-blank", 96);
+	}
+	private static File getIcon(String name, int dimension) {
+		return new File(String.format("data/%s-%d.png", name, dimension));
+	}
+
+	public static String capitalize(String str) {
+		if (str == null || str == "")
+			return "";
+		return Character.toUpperCase(str.charAt(0)) + str.substring(1);
+	}
+	
+}
