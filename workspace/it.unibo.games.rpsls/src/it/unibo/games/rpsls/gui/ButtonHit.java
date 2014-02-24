b6d62e57b89f7158ad669c5e944eb67327e02065
Leo Iannacone <l3on@ubuntu.com>
2014-02-24 15:33:43 +0100

    testing action performed

--- workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/gui/ButtonHit.java
+++ workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/gui/ButtonHit.java
@@ -4,9 +4,12 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
+import it.unibo.games.rpsls.game.Hit;
 import it.unibo.games.rpsls.interfaces.IHit;
 
 import javax.imageio.ImageIO;
@@ -15,19 +18,29 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 
-public class ButtonHit extends JButton {
+public class ButtonHit extends JButton implements ActionListener {
 
 	private IHit hit;
+	private PanelGame panelGame;
 	
 	public ButtonHit(IHit hit) {
 		super();
 		this.hit = hit;
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		init();
+		addActionListener(this);
 	}
 	
 	public IHit getHit() {
 		return hit;
+	}
+	
+	public void setPanelGame(PanelGame panelGame) {
+		this.panelGame = panelGame;
+	}
+	
+	public boolean equals(ButtonHit b) {
+		return this.hit.equals(b.getHit());
 	}
 	
 	private void init() {
@@ -48,5 +61,38 @@ public class ButtonHit extends JButton {
 		this.setPreferredSize(new Dimension(50,50));
 		this.setMargin(new Insets(1, 0, 0, 0));
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(panelGame == null) 
+			return;
+		panelGame.clickedButtonHit(this);
+		
+		// only for test
+		TestThread te = new TestThread(panelGame);
+		te.start();
+	}
 	
+}
+
+class TestThread extends Thread {
+	private PanelGame p;
+	public TestThread (PanelGame p) {
+		this.p = p;
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(3000);
+			p.receivedGuestHit(new Hit("spock"));
+			Thread.sleep(3000);
+			p.clean();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
