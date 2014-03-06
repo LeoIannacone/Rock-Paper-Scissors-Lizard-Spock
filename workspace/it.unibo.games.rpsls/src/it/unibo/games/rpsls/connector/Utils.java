c8c27edeae8400cc57c3e8aad2c3c5c74cbee60e
Leo Iannacone <l3on@ubuntu.com>
2014-03-06 17:06:37 +0100

    removed useless method

--- workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/connector/Utils.java
+++ workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/connector/Utils.java
@@ -1,13 +1,5 @@
 package it.unibo.games.rpsls.connector;
 
-import java.util.Vector;
-
 public class Utils {
-	public static Vector<String> toVector(String subject, String predicate, String object) {
-		Vector<String> result = new Vector<String>();
-		result.add(subject);
-		result.add(predicate);
-		result.add(object);
-		return result;
-	}
+	
 }
