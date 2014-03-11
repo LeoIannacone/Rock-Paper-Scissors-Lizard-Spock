b66494a8884751cb97c6fadebe3239e2f8b6eb4b
Carlo Antenucci <carlo.antenucci@gmail.com>
2014-03-12 00:26:28 +0100

    created utility to generate select-where SPARQL query

--- workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/connector/Utils.java
+++ workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/connector/Utils.java
@@ -2,4 +2,28 @@ package it.unibo.games.rpsls.connector;
 
 public class Utils {
 	
+	public static String createSPARQLQuerySelectWhere(String subject, String predicate, String object){
+		
+		String select = "SELECT ";
+		String where = "WHERE { " ;
+		
+		if (subject == null){
+			select += "?subject ";
+			where += "?subject ";
+		}else
+			where += "<" + subject + "> ";
+		if (predicate == null){
+			select += "?predicate ";
+			where += "?predicate ";
+		}else
+			where += "<" + predicate + "> ";
+		if (object == null){
+			select += "?object ";
+			where += "?object ";
+		}else
+			where += "<" + object + "> ";
+		where += "}";
+		return select+where;
+	}
+	
 }
