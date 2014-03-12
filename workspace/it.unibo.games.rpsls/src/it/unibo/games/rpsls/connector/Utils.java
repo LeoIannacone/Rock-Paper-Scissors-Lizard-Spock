b5af18aa8fb6a733d048494bb141d0e4304353a8
Carlo Antenucci <carlo.antenucci@gmail.com>
2014-03-12 10:54:16 +0100

    refactoring: renamed createSPARQLQuerySelectWhere in createSimpleSPARQLQuerySelectWhere

--- workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/connector/Utils.java
+++ workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/connector/Utils.java
@@ -2,7 +2,7 @@ package it.unibo.games.rpsls.connector;
 
 public class Utils {
 	
-	public static String createSPARQLQuerySelectWhere(String subject, String predicate, String object){
+	public static String createSimpleSPARQLQuerySelectWhere(String subject, String predicate, String object){
 		
 		String select = "SELECT ";
 		String where = "WHERE { " ;
