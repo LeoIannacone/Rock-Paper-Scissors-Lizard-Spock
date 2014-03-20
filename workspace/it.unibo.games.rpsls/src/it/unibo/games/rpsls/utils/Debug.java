473439e4cbfd19fbeb79abee20624303c030a423
Carlo Antenucci <carlo.antenucci@gmail.com>
2014-03-20 14:09:19 +0100

    start working on debug

---
+++ workspace/it.unibo.games.rpsls/src/it/unibo/games/rpsls/utils/Debug.java
@@ -0,0 +1,18 @@
+package it.unibo.games.rpsls.utils;
+
+public class Debug {
+	
+	/**
+	 * 0 - disable
+	 * 1 - interaction
+	 * 2 - SIB debug
+	 */
+	
+	public static int LEVEL = 0;
+	
+	public static void print(int level, String message){
+		if (level<=LEVEL){
+			System.out.println(String.format(" debug [%d] - %s", level, message));
+		}
+	}
+}
