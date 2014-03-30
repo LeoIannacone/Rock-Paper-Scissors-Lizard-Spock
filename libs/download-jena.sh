b80ca5e145d2068b3a1752302e6c4cffc8e5bd8e
Leo Iannacone <leo.iannacone@gmail.com>
2014-03-30 23:32:36 +0200

    do not bring jena libriaries through this repository. use script libs/download-jena.sh to get them.

---
+++ libs/download-jena.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Use this script to get apache-jena jars
+
+BASE_URL="http://archive.apache.org/dist/jena/binaries"
+JENA_VER="2.11.1"
+FILENAME="apache-jena-${JENA_VER}.tar.gz"
+
+echo "Downloading jena ${JENA_VER} ..."
+curl -o ${FILENAME} ${BASE_URL}/${FILENAME}
+
+echo "Extracting ${FILENAME} ..."
+tar xzf ${FILENAME}
+
+if [ ! -d jena ] ; then mkdir jena ; fi
+mv apache-jena*/lib/*jar jena/
+
+echo "Cleaning directory ..."
+rm -r apache-jena*
+
+echo "Done."
